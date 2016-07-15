03bfb0f7efa051ee1b336c8735f4583f6737da37
Eric <ejohnson1028@gmail.com>
2016-07-15 10:00:12 -0700

    Completed .add() .delete() and .print()
    Completed adding functionality and testing for .add() .delete() and
    .print().

--- src/lists/Array.java
+++ src/lists/Array.java
@@ -1,33 +1,96 @@
 package lists;
 
 public class Array implements List {
-    private int size = 0;
+    private int size;
+    private String[] array = new String[size];
     
+    @Override
     public int size() {
         return size;
     }
     
+    @Override
     public boolean isEmpty() {
         return size == 0;     
     }
     
+    @Override
     public String get(int index){
-        return null;
+        return array[index];
     }
     
+    @Override
     public void add(String value, int index){
+        if (size == 0 && index == 0) {
+            array = new String[1];
+            array[index] = value;
+            size++;
+            return;
+        }
+        // New array to move values
+        String[] temp = new String[array.length];
+        temp = array;
         
+        array = new String[size + 1];
+
+        for (int x = 0; x < index; x++) {
+            array[x] = temp[x];
+        }
+        array[index] = value;
+        for (int x = index + 1; x < array.length; x++) {
+            // Have to subtract 1 from temp index because of the new value added
+            array[x] = temp[x - 1];
+        }
+        size++;
     }
     
+    @Override
     public void add(String value){
-       
+        if (size == 0) {
+            array = new String[1];
+            array[0] = value;
+            size++;
+            return;
+        }
+        // New array to move values
+        String[] temp = new String[array.length];
+        temp = array;
+        // Resizing my class array
+        array = new String[size + 1];
+        // Moving values back
+        for (int x = 0; x < temp.length; x++) {
+            array[x] = temp[x];
+        }
+        // Adding new value to the array
+        array[array.length - 1] = value;
+        size++;
     }
     
+    @Override
     public void delete(int index){
+        // New array to move values
+        String[] temp = new String[array.length];
+        temp = array;
+        // Resizing array to avoid memory issues
+        array = new String[size - 1];
         
+        for (int x = 0; x < index; x++) {
+            array[x] = temp[x];
+        }
+        for (int x = index; x < array.length; x++) {
+            array[x] = temp[x + 1];
+        }
+        size--;
     }
     
+    @Override
     public void clear() {
-        
+        this.array = null;
+    }
+    
+    public void print() {
+        for (int x = 0; x < array.length; x++) {
+            System.out.println(array[x]);
+        }
     }
 }
--- test/ArrayTest.java
+++ test/ArrayTest.java
@@ -14,8 +14,77 @@ public class ArrayTest {
     public void isEmpty() {
         Array array = new Array();
         
-        assertTrue(array.isEmpty());
+        assertTrue(array.isEmpty());   
     }
     
+    @Test
+    public void add() {
+        Array array = new Array();
+        
+        array.add("hello", 0);
+        assertEquals(1, array.size());
+        assertEquals("hello", array.get(0));
+        
+        array.add("goodbye");
+        assertEquals(2, array.size());
+        assertEquals("hello", array.get(0));
+        assertEquals("goodbye", array.get(1));
+        
+        array.add("welcome", 1);
+        assertEquals(3, array.size());
+        assertEquals("hello", array.get(0));
+        assertEquals("welcome", array.get(1));
+        assertEquals("goodbye", array.get(2));
+
+        array.add("first", 0);
+        assertEquals("first", array.get(0));
+        assertEquals("hello", array.get(1));
+        assertEquals("welcome", array.get(2));
+        assertEquals("goodbye", array.get(3));
+        
+        array.add("last", 4);
+        assertEquals("first", array.get(0));
+        assertEquals("hello", array.get(1));
+        assertEquals("welcome", array.get(2));
+        assertEquals("goodbye", array.get(3));
+        assertEquals("last", array.get(4));
+    }
     
+    @Test
+    public void delete() {
+        Array array = new Array();
+        
+        array.add("1");
+        array.add("2");
+        array.add("3");
+        array.add("4");
+        array.add("5");
+        
+        assertEquals(5, array.size());
+        assertEquals("1", array.get(0));
+        assertEquals("2", array.get(1));
+        assertEquals("3", array.get(2));
+        assertEquals("4", array.get(3));
+        assertEquals("5", array.get(4));
+        
+        array.delete(0);
+        assertEquals(4, array.size());
+        assertEquals("2", array.get(0));
+        assertEquals("3", array.get(1));
+        assertEquals("4", array.get(2));
+        assertEquals("5", array.get(3));
+        
+        array.delete(2);
+        assertEquals(3, array.size());
+        assertEquals("2", array.get(0));
+        assertEquals("3", array.get(1));
+        assertEquals("5", array.get(2));
+        
+        array.delete(2);
+        assertEquals(2, array.size());
+        assertEquals("2", array.get(0));
+        assertEquals("3", array.get(1));
+        
+        array.print();
+    }
 }
