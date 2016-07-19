76a39ea19f4944f7e0ddf83ec45d9661b025609c
Eric <ejohnson1028@gmail.com>
2016-07-18 20:33:41 -0700

    Removed Size Variable
    Reworked Array class so the Size variable (Which was redundant) is no
    longer needed.

--- src/lists/Array.java
+++ src/lists/Array.java
@@ -1,17 +1,16 @@
 package lists;
 
 public class Array implements List {
-    private int size;
-    private String[] array = new String[size];
+    private String[] array = new String[0];
     
     @Override
     public int size() {
-        return size;
+        return array.length;
     }
     
     @Override
     public boolean isEmpty() {
-        return size == 0;     
+        return array.length == 0;     
     }
     
     @Override
@@ -22,17 +21,16 @@ public class Array implements List {
     
     @Override
     public void add(String value, int index){
-        if (size == 0 && index == 0) {
+        if (array.length == 0 && index == 0) {
             array = new String[1];
             array[index] = value;
-            size++;
             return;
         }
         checkBounds(index);
         // New array to move values
         String[] temp = array;
         
-        array = new String[size + 1];
+        array = new String[array.length + 1];
 
         for (int x = 0; x < index; x++) {
             array[x] = temp[x];
@@ -42,28 +40,25 @@ public class Array implements List {
             // Have to subtract 1 from temp index because of the new value added
             array[x] = temp[x - 1];
         }
-        size++;
     }
     
     @Override
     public void add(String value){
-        if (size == 0) {
+        if (array.length == 0) {
             array = new String[1];
             array[0] = value;
-            size++;
             return;
         }
         // New array to move values
         String[] temp = array;  
         // Resizing my class array
-        array = new String[size + 1];
+        array = new String[array.length + 1];
         // Moving values back
         for (int x = 0; x < temp.length; x++) {
             array[x] = temp[x];
         }
         // Adding new value to the array
         array[array.length - 1] = value;
-        size++;
     }
     
     @Override
@@ -72,7 +67,7 @@ public class Array implements List {
         // New array to move values
         String[] temp = array;
         // Resizing array to avoid memory issues
-        array = new String[size - 1];
+        array = new String[array.length - 1];
         
         for (int x = 0; x < index; x++) {
             array[x] = temp[x];
@@ -80,7 +75,6 @@ public class Array implements List {
         for (int x = index; x < array.length; x++) {
             array[x] = temp[x + 1];
         }
-        size--;
     }
     
     @Override
@@ -89,36 +83,14 @@ public class Array implements List {
     }
     
     public void print() {
-        for (int x = 0; x < size; x++) {
+        for (int x = 0; x < array.length; x++) {
             System.out.println(array[x]);
         }
     }
     
     private void checkBounds(int index) {
-        if ( size == 0 || index > size || index < 0) {
+        if ( array.length == 0 || index > array.length || index < 0) {
             throw new IndexOutOfBoundsException();
-        }
-    }
-    
-    private boolean isArrayResizeNeeded() {
-        return array.length == size || array.length - 1 == size || array.length < size || array.length > size * size;
-    }
-    
-    private void resizeArray() {
-        if (size == 0) {
-            array = new String[1];
-            return;
-        }
-        String[] temp = array;
-        array = new String[size + size/2];
-        
-        // Will reassign all values from temp array into the newly sized array
-        // All new empty positions will be "0" (I will be keeping track of size seperatly)
-        for (int x = 0; x < array.length; x++) {
-            if (x < temp.length){
-                array[x] = temp[x];
-            }
-            array[x] = "0";
         }
     }
 }
